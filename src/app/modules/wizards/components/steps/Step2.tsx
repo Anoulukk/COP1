470ef1bcadd01e2c88cb4b3f384e9302b9c70695
Anoulukk <101563242+Anoulukk@users.noreply.github.com>
2024-10-15 21:42:04 +0700

    step 2 70%

--- src/app/modules/wizards/components/steps/Step2.tsx
+++ src/app/modules/wizards/components/steps/Step2.tsx
@@ -1,24 +1,68 @@
 import {FC} from 'react'
 import {KTIcon} from '../../../../../_metronic/helpers'
 import {ErrorMessage, Field} from 'formik'
+import DynamicTable from '../DynamicTable';
 import Select from 'react-select'
 const Step2: FC = () => {
-  const form210A = [
+  const form210 = [
     { classified: "sub_head", code: "210A1", description: "ຖະໜົນ", input_type: "text" },
     { classified: "sub_head", code: "210A2", description: "ບ້ານ", input_type: "choice" },
     { classified: "sub_head", code: "210A3", description: "ເມືອງ", input_type: "choice" },
     { classified: "sub_head", code: "210A4", description: "ແຂວງ", input_type: "choice" },
     { classified: "sub_head", code: "210A5", description: "ເຂດເສດຖະກິດພິເສດ", input_type: "choice" },
-  ]
-  const form210B = [
     { classified: "title", code: "210B", description: "ແຜນທີ່ໂຮງງານ", input_type: "file" },
-    
-  ]
-  const form210C = [
     { classified: "title", code: "210C", description: "ຕຳແໜ່ງພິກັດ GPS (ລະບົບ UTM)", input_type: "choice" },
     { classified: "sub_head", code: "210C1", description: "Zone", input_type: "text" },
     { classified: "sub_head", code: "210C2", description: "ພິກັດ Easting: E", input_type: "text" },
     { classified: "sub_head", code: "210C3", description: "ພິກັດ Northing: N", input_type: "text" },   
+  ]
+  const form220 = [
+    { classified: "title", code: "220A", description: "ພື້ນທີ່ຕິດກັບໂຮງງານ", input_type: null },
+    { classified: "sub_head", code: "220A1", description: "ທິດເໜືອ", input_type: "text" },
+    { classified: "sub_head", code: "220A2", description: "ທິດໃຕ້", input_type: "text" },
+    { classified: "sub_head", code: "220A3", description: "ທິດຕາເວັນອອກ", input_type: "text" },
+    { classified: "sub_head", code: "220A4", description: "ທິດຕາເວັນຕົກ", input_type: "text" },
+
+    { classified: "title", code: "220B", description: "ແຜນຜັງສະແດງໃຫ້ເຫັນທີ່ຕັ້ງໂຮງງານ ໃນຂອບເຂດລັດສະໝີ 500 ແມັດ", input_type: "file" },
+    { classified: "title", code: "220C", description: "ພື້ນທີ່ອ້ອມຂ້າງໂຮງງານ ", input_type: null },
+    { classified: "sub_head", code: "220C1", description: "ຊຸມຊົນ", input_type: "text" },
+    { classified: "sub_head", code: "220C2", description: "ແຫຼ່ງນໍ້າ", input_type: "text" },
+    { classified: "sub_head", code: "220C3", description: "ໂຮງຮຽນ", input_type: "text" },
+    { classified: "sub_head", code: "220C4", description: "ໂຮງໝໍ ຫຼື ສຸກສາລາ", input_type: "text" },
+    { classified: "sub_head", code: "220C5", description: "ວັດວາອາຮາມ", input_type: "text" },
+    { classified: "sub_head", code: "220C6", description: "ສະຖານທີ່ປະຫວັດສາດ ຫຼື ບູຮານວັດຖຸ", input_type: "text" },
+    { classified: "sub_head", code: "220C7", description: "ເຂດອະນຸລັກຊີວະນານາພັນ", input_type: "text" },
+    { classified: "sub_head", code: "220C8", description: "ແຫຼ່ງທ່ອງທ່ຽວ", input_type: "text" },
+    { classified: "sub_head", code: "220C9", description: "ອື່ນໆ (ລະບຸ)", input_type: "text" }, 
+  ]
+
+  const form230 = [
+    { classified: "sub_head", code: "231A", description: "ເນື້ອທີ່ທັງໝົດ (ຕາແມັດ)", input_type: "number" },
+    { classified: "sub_head", code: "231B", description: "ພື້ນທີ່ສີຂຽວ (ສ່ວນຮ້ອຍ)", input_type: "number" },
+    { classified: "sub_head", code: "231C", description: "ການນຳໃຊ້ພື້ນທີ່", input_type: "T2-1" },
+    { classified: "sub_head", code: "231D", description: "ແຜນຜັງການນຳໃຊ້ພື້ນທີ່", input_type: "file" },
+
+    { classified: "title", code: "232", description: "ອາຄານ ແລະ ສິ່ງປຸກສ້າງ", input_type: null },
+    { classified: "sub_head", code: "232A", description: "ຈຳນວນອາຄານ ແລະ ສິ່ງປຸກສ້າງ", input_type: "text" },
+    { classified: "sub_head", code: "232B", description: "ຂໍ້ມູນອາຄານ ແລະ ສິ່ງແວດລ້ອມ", input_type: "T2-2" },
+    { classified: "sub_head", code: "232C", description: "ໃບອະນຸຍາດປຸກສ້າງໂຮງງານ ເລກທີ", input_type: "text" },
+
+    { classified: "title", code: "232D", description: "ແຜນຜັງການກໍ່ສ້າງ ແລະ ລະບົບເຕັກນິກໂຮງງານ", input_type: null },
+    { classified: "sub_head", code: "232D1", description: "ແຜນຜັງສະແດງສິ່ງປຸກສ້າງໃນບໍລິເວນໂຮງງານ", input_type: "file" },
+    { classified: "sub_head", code: "232D2", description: "ແຜນຜັງກໍ່ສ້າງອາຄານຜະລິດ (ຮູບດ້ານໜ້າ, ຂ້າງ, ຫຼັງ ແລະ ຮູບຕັດ)", input_type: "file" },
+    { classified: "sub_head", code: "232D3", description: "ແຜນຜັງການກໍ່ສ້າງສາງເກັບມ້ຽນເຄມີ", input_type: "file" },  
+  ]
+  const form240 = [
+    { classified: "sub_head", code: "240A", description: "ຫ້ອງເກັບສານເຄມີ", input_type: "text" },
+    { classified: "sub_head", code: "240B", description: "ຫ້ອງເກັບສິ່ງເສດເຫຼືອເຄມີ", input_type: "text" },
+    { classified: "sub_head", code: "240C", description: "ອາຄານ ຫຼື ພື້ນທີ່ເກັບວັດຖຸດິບ", input_type: "text" },
+    { classified: "sub_head", code: "240D", description: "ອາຄານ ຫຼື ພື້ນທີ່ເກັບເຊື້ອໄຟ", input_type: "text" },
+  ]
+  const form250 = [
+    { classified: "sub_head", code: "250A", description: "ໃບອະນຸຍາດນຳໃຊ້ທີ່ດິນ", input_type: "text" },
+    { classified: "sub_head", code: "250B", description: "ໃບອະນຸຍາດປຸກສ້າງໂຮງງານ", input_type: "text" },
+    { classified: "sub_head", code: "250C", description: "ໃບອະນຸຍາດສ້າງສາງເກັບມ້ຽນເຄມີ", input_type: "text" },
+
   ]
 
   const form200 = [
@@ -88,7 +132,9 @@ const Step2: FC = () => {
     const renderInput = (inputType:any, description:any) => {
       switch (inputType) {
         case 'text':
-          return <input type="text" placeholder={description} className="form-control" />;
+        case 'number':
+        case 'file':
+          return <input type={inputType} placeholder={description} className="form-control" />;
         case 'choice':
           return (
             <Select  
@@ -98,9 +144,12 @@ const Step2: FC = () => {
             />
 
           );
-        case 'file':
-          return <input type="file" placeholder={description} className="form-control" />;
-        default:
+          case 'T2-1':
+            return (
+              <DynamicTable />
+            );
+      
+         default:
           return null;
       }
     };
@@ -110,7 +159,8 @@ const Step2: FC = () => {
     <div className=''  style={{ width: '100%' }}>
       <h2 className='mb-5'>
 
-      200 ທີ່ຕັ້ງຂອງໂຮງງານ
+      200 ທີ່ຕັ້ງ ແລະ ການນຳໃຊ້ພື້ນທີ່ໂຮງງານ
+
   </h2>
 <div className='accordion' id='kt_accordion_1'>
 <div className='accordion-item'>
@@ -123,7 +173,8 @@ const Step2: FC = () => {
       aria-expanded='false'
       aria-controls='kt_accordion_1_body_1'
     >
-      210A ທີ່ຕັ້ງໂຮງງານ
+      210 ທີ່ຕັ້ງຂອງໂຮງງານ
+
     </button>
   </h2>
   <div
@@ -133,10 +184,11 @@ const Step2: FC = () => {
     data-bs-parent='#kt_accordion_1'
   >
     <div className='accordion-body'>
-        {form210A.map((item, index) => {
+        {form210.map((item, index) => {
           return (
             <div className="form-group mb-3" key={index}>
-              <label>{item.description}</label>
+              {item.classified === "title" ? <h4>{item.code} {item.description}</h4> 
+              :(<span className='fs-5'>{item.code} {item.description}</span>)}
               {renderInput(item.input_type, item.description)}
             </div>
           );
@@ -154,7 +206,8 @@ const Step2: FC = () => {
       aria-expanded='false'
       aria-controls='kt_accordion_1_body_2'
     >
-      210B ທີ່ຕັ້ງໂຮງງານ
+      220 ສະພາບພື້ນທີ່ອ້ອມຂ້າງໂຮງງານ
+
     </button>
   </h2>
   <div
@@ -164,10 +217,11 @@ const Step2: FC = () => {
     data-bs-parent='#kt_accordion_1'
   >
     <div className='accordion-body'>
-    {form210B.map((item, index) => {
+    {form220.map((item, index) => {
           return (
             <div className="form-group mb-3" key={index}>
-              <label>{item.description}</label>
+              {item.classified === "title" ? <h4>{item.code} {item.description}</h4> 
+              :(<span className='fs-5'>{item.code} {item.description}</span>)}
               {renderInput(item.input_type, item.description)}
             </div>
           );
@@ -185,7 +239,8 @@ const Step2: FC = () => {
       aria-expanded='false'
       aria-controls='kt_accordion_1_body_3'
     >
-      220C 
+      230	ການນຳໃຊ້ພື້ນທີ່, ອາຄານ ແລະ ສິ່ງປຸກສ້າງພາຍໃນໂຮງງານ
+
     </button>
   </h2>
   <div
@@ -195,13 +250,15 @@ const Step2: FC = () => {
     data-bs-parent='#kt_accordion_1'
   >
     <div className='accordion-body'>
-      <strong>This is the third item's accordion body.</strong>It is hidden by
-      default, until the collapse plugin adds the appropriate classes that we use to
-      style each element. These classes control the overall appearance, as well as the
-      showing and hiding via CSS transitions. You can modify any of this with custom
-      CSS or overriding our default variables. It's also worth noting that just about
-      any HTML can go within the
-      <code>.accordion-body</code>, though the transition does limit overflow.
+    {form230.map((item, index) => {
+          return (
+            <div className="form-group mb-3" key={index}>
+              {item.classified === "title" ? <h4>{item.code} {item.description}</h4> 
+              :(<span className='fs-5'>{item.code} {item.description}</span>)}
+              {renderInput(item.input_type, item.description)}
+            </div>
+          );
+      })}
     </div>
   </div>
 </div>
@@ -215,7 +272,8 @@ const Step2: FC = () => {
       aria-expanded='false'
       aria-controls='kt_accordion_1_body_4'
     >
-      140 ຫົວໜ່ວຍຄວາມປອດໄພ ແລະ ສຸຂະພາບແຮງງານ
+      240	ອາຄານ ແລະ ພື້ນທີ່ຄວບຄຸມ 
+
     </button>
   </h2>
   <div
@@ -225,13 +283,48 @@ const Step2: FC = () => {
     data-bs-parent='#kt_accordion_1'
   >
     <div className='accordion-body'>
-      <strong>This is the third item's accordion body.</strong>It is hidden by
-      default, until the collapse plugin adds the appropriate classes that we use to
-      style each element. These classes control the overall appearance, as well as the
-      showing and hiding via CSS transitions. You can modify any of this with custom
-      CSS or overriding our default variables. It's also worth noting that just about
-      any HTML can go within the
-      <code>.accordion-body</code>, though the transition does limit overflow.
+    {form240.map((item, index) => {
+          return (
+            <div className="form-group mb-3" key={index}>
+              {item.classified === "title" ? <h4>{item.code} {item.description}</h4> 
+              :(<span className='fs-5'>{item.code} {item.description}</span>)}
+              {renderInput(item.input_type, item.description)}
+            </div>
+          );
+      })}
+    </div>
+  </div>
+</div>
+<div className='accordion-item'>
+  <h2 className='accordion-header' id='kt_accordion_1_header_5'>
+    <button
+      className='accordion-button fs-4 fw-bold collapsed'
+      type='button'
+      data-bs-toggle='collapse'
+      data-bs-target='#kt_accordion_1_body_5'
+      aria-expanded='false'
+      aria-controls='kt_accordion_1_body_5'
+    >
+      250	ເອກະສານທີ່ກ່ຽວຂ້ອງ
+
+    </button>
+  </h2>
+  <div
+    id='kt_accordion_1_body_5'
+    className='accordion-collapse collapse'
+    aria-labelledby='kt_accordion_1_header_5'
+    data-bs-parent='#kt_accordion_1'
+  >
+    <div className='accordion-body'>
+    {form250.map((item, index) => {
+          return (
+            <div className="form-group mb-3" key={index}>
+              {item.classified === "title" ? <h4>{item.code} {item.description}</h4> 
+              :(<span className='fs-5'>{item.code} {item.description}</span>)}
+              {renderInput(item.input_type, item.description)}
+            </div>
+          );
+      })}
     </div>
   </div>
 </div>
